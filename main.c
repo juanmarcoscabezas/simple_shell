21450210053760ceaddcd0c8e1be556362e5a360
hb4y <hugobaymon@gmail.com>
2019-11-15 15:07:12 +0000

    init the main for first test

--- main.c
+++ main.c
@@ -1,4 +1,15 @@
-int main(void)
+#include "shell.h"
+
+/**
+ * main.c - function main entry.
+ * Description: main function for our shell
+ * @argc: number of arguments pass to the shell
+ * @argv: char ** pass to the shell
+ * @envp: enviroment pass to the shell
+ * Return: 0 on success
+ **/
+int main(int argc, char *argv[], char *envp[])
 {
-	
+	print_prompt();
+	return (0);
 }
