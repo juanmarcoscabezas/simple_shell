ce6f4a289929b55764507ad59202a3bcc8de24b6
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-15 15:02:52 +0000

    Adding get_commands function

---
+++ get_commands.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+ * get_commands - Gets the commands
+ * Description: This function gets commands
+ * @void
+ * Return: Always 0
+ */
+int get_commands(void)
+{
+	return (0);
+}
--- print_prompt.c
+++ print_prompt.c
@@ -1,5 +1,10 @@
 #include "shell.h"
-
+/**
+ * print_prompt - Prints a prompt
+ * Description: This function prints a prompt
+ * @void:
+ * Return:
+ */
 void print_prompt(void)
 {
 	printf("$ ");	
