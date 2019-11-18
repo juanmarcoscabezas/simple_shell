830cc93efa9163e0cbe11e05502824d581709043
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-18 20:47:12 +0000

    Adding PATH

--- execute_commands.c
+++ execute_commands.c
@@ -9,11 +9,20 @@
  */
 int execute_commands(char *argv[], char *envp[])
 {
-	int execution, status;
+	int execution, status, is_accessible;
 	pid_t pid;
+	char *path;
+	char **directories;
+
+	/* Check if the file exists */
+	is_accessible = access(argv[0], F_OK);
+	if (is_accessible == -1)
+	{
+		path = getenv("PATH");
+		
+	}
 
 	pid = fork();
-
 	if (pid == 0)
 	{
 		execution = execve(argv[0], argv, envp);
