5569b54fe72fede6f01abaa07ec535f29df873c0
hb4y <hugobaymon@gmail.com>
2019-11-20 15:20:57 +0000

    refactor for style issues

--- get_commands.c
+++ get_commands.c
@@ -2,11 +2,14 @@
 
 /**
  * get_commands - Gets the commands
- * Description: This function gets commands
- * @void
+ * Description: This function gets commands & break them in tokens
+ * @cp_argv: Copy of the original argv (the name of the executable)
+ * @buffer: Params
+ * @envp: Enviroment params
+ * @n_commands: the number of commands given until this point.
  * Return: Always 0
  */
-int get_commands(char *cp_argv[], char *buffer, char *envp[], int *number_commands)
+int get_commands(char *cp_argv[], char *buffer, char *envp[], int *n_commands)
 {
 	char *token;
 	char **tokens = malloc(sizeof(char *) * 64);
@@ -21,6 +24,6 @@ int get_commands(char *cp_argv[], char *buffer, char *envp[], int *number_comman
 		token = strtok(NULL, " \n");
 	}
 	tokens[pos] = NULL;
-	execute_commands(cp_argv, tokens, envp, number_commands);
+	execute_commands(cp_argv, tokens, envp, n_commands);
 	return (0);
 }
