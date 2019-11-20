fa3a263181d8a69de2ba99e8ac408d30cd6c80a6
Juan Marcos Cabezas <1225@holbertonschool.com>
2019-11-20 15:55:43 +0000

    Adding multiple commands

--- get_stdin.c
+++ get_stdin.c
@@ -1,6 +1,24 @@
 #include "shell.h"
 #define LSH_TOK_DELIM " \t\r\n\a"
 
+void process_command(char *cp_argv[], char *envp[], int *number_commands, char *command)
+{
+	char *token;
+	char **tokens = malloc(sizeof(char *) * 64);
+	int pos = 0;
+
+	token = strtok(command, LSH_TOK_DELIM);
+	while (token)
+	{
+		tokens[pos] = token;
+		pos++;
+		token = strtok(NULL, LSH_TOK_DELIM);
+	}
+	tokens[pos] = NULL;
+	execute_commands(cp_argv, tokens, envp, number_commands);
+	free(tokens);
+}
+
 /**
  * get_stdin.c - function to get stdin.
  * Description: main 
@@ -11,27 +29,19 @@
  **/
 void get_stdin(char *cp_argv[], char *envp[], int *number_commands)
 {
-	char auxiliar[100];
+	char *command = malloc(sizeof(char) * 1024);
 	int pos = 0;
 	char ch;
-	char **tokens = malloc(sizeof(char *) * 64);
-	char *token;
 
 	while(read(STDIN_FILENO, &ch, 1) > 0)
 	{
-		auxiliar[pos] = ch;
+		command[pos] = ch;
+		if (ch == '\0' || ch == '\n')
+		{
+			process_command(cp_argv, envp, number_commands, command);
+			pos = -1;
+		}
 		pos++;
 	}
-	auxiliar[pos] = '\0';
-
-	pos = 0;
-	token = strtok(auxiliar, LSH_TOK_DELIM);
-	while(token)
-	{
-		tokens[pos] = token;
-		pos++;
-		token = strtok(NULL, LSH_TOK_DELIM);
-	}
-	tokens[pos] = NULL;
-	execute_commands(cp_argv, tokens, envp, number_commands);
+	free(command);
 }
